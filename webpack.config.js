7259cb1755e659c53f20c5210ef485054fc41d31
Benjamin DeLong <benjamin.delong@vineview.com>
2020-07-01 01:23:17 -0300

    fix build issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,44 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
 
 var config = {
-	entry : {
-		main: ['./src/index.js', './example/js/script.js']
-	},
-	output: {
-		path: path.resolve(__dirname, 'example', 'dist'),
-		filename: '[name].js',
-		publicPath: '/example'
-	},
-	module: {
-		rules: [
-			{ 
-				test: /\.(js)$/, 
-				use: 'babel-loader' 
-			},
-			{ 
-				test: /\.css$/, 
-				use: ['style-loader', 'css-loader']
-			}
-		]
-	}, 
-	devServer: {
-		historyApiFallback: true,
-		hot: true
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'example/index.html'
-		}),
-		new webpack.HotModuleReplacementPlugin()
-	]
-}
+  entry: {
+    main: ["./src/index.js", "./example/js/script.js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "example", "dist"),
+    filename: "[name].js",
+    publicPath: "/example",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    before: function (app, server) {
+      app.get("/", function (req, res) {
+        res.redirect("/example");
+      });
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "example/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
 
 module.exports = config;
 
